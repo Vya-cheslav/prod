18052f7e12fe977376f07a2d76b6083b88320454
Vya-cheslav <39085011+Vya-cheslav@users.noreply.github.com>
2019-12-23 15:32:30 +0300

    Add files via upload

---
+++ prod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prod
+//
+//  Created by Вячеслав Рожнов on 23.12.2019.
+//  Copyright © 2019 Вячеслав Рожнов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
